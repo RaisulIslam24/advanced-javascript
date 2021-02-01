9ec2e008c2e06f25b25fccd3cd70184db5d4f6ed
Raisul Islam Nibir <raisulislam.cse3.bu@gmail.com>
2021-02-01 22:47:38 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1+num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"Nibir", phone:169535};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 3];
+console.log(ages[11]);
